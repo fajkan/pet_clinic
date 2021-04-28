c46a5712e0525eb49ed741eb993a6f94af33a18f
Armin <fajkan10@gmail.com>
2021-04-28 11:37:28 +0200

    Implemented service layer methods

--- src/main/java/se/lexicon/pet_clinic/service/PetServiceImpl.java
+++ src/main/java/se/lexicon/pet_clinic/service/PetServiceImpl.java
@@ -1,6 +1,39 @@
 package se.lexicon.pet_clinic.service;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.lexicon.pet_clinic.dto.PetDto;
+import se.lexicon.pet_clinic.repository.PetRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PetServiceImpl {
-    // todo: implement PetService Interface
+
+    PetRepository petRepository;
+    ModelMapper modelMapper;
+
+    @Autowired
+    public void setPetRepository(PetRepository petRepository){
+        this.petRepository = petRepository;
+    }
+
+    @Autowired
+    public void setModelMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    private List<PetDto> findByPetTypeName(String petTypeName){
+        return petRepository.findByPetType_NameIgnoreCase(petTypeName).stream().map(pet -> modelMapper.map(pet,PetDto.class)).collect(Collectors.toList());
+    }
+
+    private List<PetDto> findByOwnerFirstNameAndLastName(String firstName, String lastName){
+        return petRepository.findByOwner_FirstNameIgnoreCaseAndOwner_LastNameIgnoreCase(firstName,lastName).stream().map(pet -> modelMapper.map(pet, PetDto.class)).collect(Collectors.toList());
+    }
+
+    private List<PetDto> findByOwnerTelephone (String Telephone){
+        return petRepository.findByOwner_Telephone(Telephone).stream().map(pet -> modelMapper.map(pet,PetDto.class)).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/se/lexicon/pet_clinic/service/PetTypeServiceImpl.java
+++ src/main/java/se/lexicon/pet_clinic/service/PetTypeServiceImpl.java
@@ -3,6 +3,8 @@ package se.lexicon.pet_clinic.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import se.lexicon.pet_clinic.dto.PetTypeDto;
+import se.lexicon.pet_clinic.entity.PetType;
 import se.lexicon.pet_clinic.repository.PetTypeRepository;
 
 
@@ -20,6 +22,16 @@ public class PetTypeServiceImpl {
     @Autowired
     public void setModelMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
+    }
+
+    //@Override
+    public PetTypeDto save(PetTypeDto dto){
+        if (dto == null) throw new IllegalArgumentException("PetTypeDto object should not be null");
+        //if (dto.getId() != null) throw new IllegalArgumentException("Id should be null");
+        PetType petTypeEntity = modelMapper.map(dto, PetType.class);
+        PetType savedPetTypeEntity = petTypeRepository.save(petTypeEntity);
+        PetTypeDto convertEntityToDto = modelMapper.map(savedPetTypeEntity, PetTypeDto.class);
+        return convertEntityToDto;
     }
 
 }
