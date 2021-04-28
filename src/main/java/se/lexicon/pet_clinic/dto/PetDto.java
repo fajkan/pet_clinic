7bf8970d23167199022f29cb3e7965943c12c98e
Armin <fajkan10@gmail.com>
2021-04-28 10:16:04 +0200

    Updated the Dto

--- src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
+++ src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
@@ -1,5 +1,16 @@
 package se.lexicon.pet_clinic.dto;
 
+import lombok.Data;
+import se.lexicon.pet_clinic.entity.Owner;
+import se.lexicon.pet_clinic.entity.PetType;
+
+import java.time.LocalTime;
+
+@Data
 public class PetDto {
-    // todo: implement PetDto
+    private String id;
+    private String name;
+    private LocalTime birthDate;
+    private PetType petType;
+    private Owner owner;
 }
--- src/main/java/se/lexicon/pet_clinic/dto/PetTypeDto.java
+++ src/main/java/se/lexicon/pet_clinic/dto/PetTypeDto.java
@@ -1,6 +1,11 @@
 package se.lexicon.pet_clinic.dto;
 
+
+import lombok.Data;
+
+@Data
 public class PetTypeDto {
-    // todo: implement PetTypeDto
+    private int id;
+    private String name;
 
 }
