b9daffc00f472dd695b30144518c4aca3476e272
Armin <fajkan10@gmail.com>
2021-04-28 10:47:04 +0200

    Defined the save, update, delete, find all and getall in the Pet Type interface

--- src/main/java/se/lexicon/pet_clinic/service/PetTypeService.java
+++ src/main/java/se/lexicon/pet_clinic/service/PetTypeService.java
@@ -1,7 +1,24 @@
 package se.lexicon.pet_clinic.service;
 
+
+import se.lexicon.pet_clinic.dto.PetTypeDto;
+import se.lexicon.pet_clinic.exception.DataNotFoundException;
+
+import java.util.List;
+
 public interface PetTypeService {
 
     // todo: define SAVE, UPDATE, DELETE, Find All, FIND BY ID
+
+    PetTypeDto save(PetTypeDto dto);
+
+    PetTypeDto update(PetTypeDto dto) throws DataNotFoundException;
+
+    void deleteById(String id);
+
+    PetTypeDto findById(String id) throws DataNotFoundException;
+
+    List<PetTypeDto> getAll();
+
 
 }
--- src/main/java/se/lexicon/pet_clinic/service/PetTypeServiceImpl.java
+++ src/main/java/se/lexicon/pet_clinic/service/PetTypeServiceImpl.java
@@ -1,7 +1,25 @@
 package se.lexicon.pet_clinic.service;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.lexicon.pet_clinic.repository.PetTypeRepository;
+
+
 public class PetTypeServiceImpl {
-    // todo: implement PetTypeService Interface
+
+
+    PetTypeRepository petTypeRepository;
+    ModelMapper modelMapper;
+
+    @Autowired
+    public void setPetTypeRepository(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Autowired
+    public void setModelMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
 }
