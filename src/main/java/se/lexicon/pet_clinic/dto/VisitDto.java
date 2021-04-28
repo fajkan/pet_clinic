6fdf345db9a14f2a1a2932b7b9b76c2c1befc6fc
Armin <fajkan10@gmail.com>
2021-04-28 10:17:45 +0200

    Updated the Visit Dto

--- src/main/java/se/lexicon/pet_clinic/dto/VisitDto.java
+++ src/main/java/se/lexicon/pet_clinic/dto/VisitDto.java
@@ -1,5 +1,14 @@
 package se.lexicon.pet_clinic.dto;
 
+import lombok.Data;
+import se.lexicon.pet_clinic.entity.Pet;
+
+import java.time.LocalDate;
+
+@Data
 public class VisitDto {
-    // todo: implement VisitDao
+    private String id;
+    private Pet pet;
+    private LocalDate visitDate;
+    private String description;
 }
